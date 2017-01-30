b91386ecbd6d138d4d893c21b4acd70ff4d5e79a
stanislav3316 <stanislav3316@gmail.com>
2017-01-30 14:24:03 +0300

    fix python2

--- hello.py
+++ hello.py
@@ -2,11 +2,10 @@ from urlparse import parse_qs, urlparse
 
 def app(environ, start_response):
 	o = parse_qs(urlparse("?" + str(environ.get('QUERY_STRING', ''))).query, keep_blank_values=True)
-	response = b''    
+	response = ''    
 	for key in list(sorted(o.keys())):
 		for val in list(o[key]): 
-			response += bytes(str(key), encoding = 'utf-8')  + bytes(str("="), encoding = 'utf-8') + \
- 			bytes(str(val), encoding = 'utf-8') + bytes(str("\n"), encoding = 'utf-8')
+			response += str(key) + "=" + str(val) + "\n"
 	status = '200 OK'
 	response_headers = [
 		('Content-Type', 'text/plain')
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo gunicorn -c /home/iters/web/etc/hello.py hello:app
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
