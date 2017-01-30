c5401ed0929d70924147c4bec41bc63ed5fa947a
stanislav3316 <stanislav3316@gmail.com>
2017-01-30 15:20:45 +0300

    yes

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn start
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
