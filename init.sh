7a4285677077c1c47ca140c531c65020e6e156b1
stanislav3316 <stanislav3316@gmail.com>
2017-01-30 14:38:58 +0300

    atm

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /home/box/web/etc/hello.py hello:app
