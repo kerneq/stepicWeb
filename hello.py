aee39885cf928a80ad0e7dae2923de13f121de11
stanislav3316 <stanislav3316@gmail.com>
2017-01-31 02:25:50 +0300

    3 trying

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ from urlparse import urlparse, parse_qs
 def app(environ, start_response):
 	o = parse_qs("?" + urlparse(str(environ.get('QUERY_STRING', ''))).query, keep_blank_values=True)
 	response = ''
+
+	print(o)
 	for key in list(sorted(o.keys())):
     		response += key + "=" + o[key][0] + "\n"
 	status = '200 OK'
