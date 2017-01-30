b808d2d45b87d5fc7f63294fb43b453d9a80c0bb
stanislav3316 <stanislav3316@gmail.com>
2017-01-30 12:51:53 +0300

    app server is ready

---
+++ hello.py
@@ -0,0 +1,14 @@
+def app(environ, start_response):
+    o = parse_qs(urlparse(environ['QUERY_STRING']).query, keep_blank_values=True)
+    response = ''
+    
+    for key in list(sorted(o.keys())):
+    	response += key + "=" + o[key][0] + "\n"
+
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, response_headers)
+
+    return [response]
