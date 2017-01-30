1fda94171073edf29e301200c49e8e776a2b0df8
stanislav3316 <stanislav3316@gmail.com>
2017-01-30 14:00:53 +0300

    fix server

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
+from urllib.parse import urlparse, parse_qs
+
 def app(environ, start_response):
-    o = parse_qs(urlparse(environ['QUERY_STRING']).query, keep_blank_values=True)
-    response = ''
+    o = parse_qs(urlparse("?" + str(environ.get('QUERY_STRING', ''))).query, keep_blank_values=True)
+    response = b''
     
     for key in list(sorted(o.keys())):
-    	response += key + "=" + o[key][0] + "\n"
+	   	response += bytes(str(key), encoding = 'utf-8') + bytes(str("="), encoding = 'utf-8') + \
+ 		bytes(str(o[key][0]), encoding = 'utf-8') + bytes(str("\n"), encoding = 'utf-8')
 
     status = '200 OK'
     response_headers = [
