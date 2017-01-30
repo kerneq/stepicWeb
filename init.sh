96afb85aa4023784af4973ad2a55bfe2f10a4d48
stanislav3316 <stanislav3316@gmail.com>
2017-01-30 15:11:16 +0300

    test

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+sudo /etc/init.d/gunicorn start
