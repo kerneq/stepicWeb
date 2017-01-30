4f50fc02de675dabbdc6a5e73d758afaf20fccc6
stanislav3316 <stanislav3316@gmail.com>
2017-01-31 02:33:29 +0300

    4 truing

--- hello.py
+++ hello.py
@@ -2,14 +2,14 @@ from urlparse import urlparse, parse_qs
 
 def app(environ, start_response):
 	o = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-	response = ''
-
-	print(o)
-	for key in list(sorted(o.keys())):
-    		response += key + "=" + o[key][0] + "\n"
+	body = []
+  	for key, values in o.items():
+		for item in values:
+			body.append(key + "=" + item + "\r\n")
+	
 	status = '200 OK'
 	response_headers = [
 		('Content-Type', 'text/plain')
 	]
 	start_response(status, response_headers)
-	return [response]
+	return body
