81fe20a8a33f135b521c5c96e54e8cee7874fe86
stanislav3316 <stanislav3316@gmail.com>
2017-02-02 00:14:45 +0300

    setup mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,6 +4,14 @@ from django.db import models
 
 # Create your models here.
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return Question.objects.order_by('-added_at')[0]
+
+    def popular(self):
+        return Question.objects.order_by('rating')
+
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
@@ -11,6 +19,7 @@ class Question(models.Model):
     rating = models.IntegerField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='+')
     likes = models.ManyToManyField(User)
+    objects = QuestionManager()
 
 
 class Answer(models.Model):
@@ -18,11 +27,3 @@ class Answer(models.Model):
     added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return Question.objects.order_by('-added_at')[0]
-
-    def popular(self):
-        return Question.objects.order_by('rating')
