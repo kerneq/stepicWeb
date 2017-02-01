2c52d624eb663e670389c64537640ca98b94fca3
stanislav3316 <stanislav3316@gmail.com>
2017-02-01 23:55:24 +0300

    start working with Models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='+')
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return Question.objects.order_by('-added_at')[0]
+
+    def popular(self):
+        return Question.objects.order_by('rating')
--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx start
+sudo /etc/init.d/mysql start﻿
 # sudo ln -sf /home/box/etc/hello.py  /etc/gunicorn.d/hello.py
 # sudo gunicorn -b 0.0.0.0:8000 /home/box/web/ask/ask.wsgi
